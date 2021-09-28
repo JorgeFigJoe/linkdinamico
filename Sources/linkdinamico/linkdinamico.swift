a9bf2e004b9ac8a2c48a484d497996f04c721848
JorgeFig <sanchezjorf@globalhitss.com>
2021-09-28 17:39:44 -0500

    pruebas

--- Sources/linkdinamico/linkdinamico.swift
+++ Sources/linkdinamico/linkdinamico.swift
@@ -24,11 +24,14 @@ public struct linkdinamico {
         print("The long URL is: \(longDynamicLink)")
         urlDefault = longDynamicLink
         
-        linkBuilder!.shorten() { url, warnings, error in
-          guard let url = url else { return }
-          print("The short URL is: \(url)")
-          urlDefault = url
+        DispatchQueue.main.async {
+            linkBuilder!.shorten() { url, warnings, error in
+              guard let url = url, error == nil else { return }
+              print("The short URL is: \(url)")
+              urlDefault = url
+            }
         }
+        
         return urlDefault
     }
 }
