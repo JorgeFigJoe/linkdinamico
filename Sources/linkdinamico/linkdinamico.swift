7312366bdfaacce7af433d449e55b08e70fc6f0f
JorgeFig <sanchezjorf@globalhitss.com>
2021-09-30 13:42:46 -0500

    add URLSession for get url short

--- Sources/linkdinamico/linkdinamico.swift
+++ Sources/linkdinamico/linkdinamico.swift
@@ -9,6 +9,7 @@ public struct linkdinamico {
     
     public func generateDynamicLinks(completion: @escaping (Result<URL,Error>) -> Void){
         
+        //Generate longDynamicLink
         guard let link = URL(string: "https://www.videoconferenciaclaro.com") else { return }
         let dynamicLinksDomainURIPrefix = "https://testvcc.page.link"
         let linkBuilder = DynamicLinkComponents(link: link, domainURIPrefix: dynamicLinksDomainURIPrefix)
@@ -21,22 +22,42 @@ public struct linkdinamico {
         linkBuilder!.socialMetaTagParameters?.descriptionText = "Enlace de descripcion"
         
         guard let longDynamicLink = linkBuilder!.url else { return }
-        //print("The long URL is: \(longDynamicLink)")
         //completion(.success(longDynamicLink))
         
         
-            linkBuilder!.shorten() { url, warnings, error in
-              //guard let url = url else { return  }
-                
-                if let url2 = url {
-                    //print("The short URL is: \(url2)")
-                      completion(.success(url2))
-                }else{
-                   // print("The long URL is: \(longDynamicLink)")
-                      completion(.success(longDynamicLink))
-                }
-            }
+        //generate ShortDynamicLink
+        let url = URL(string: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyA2FuB7RvppYnJ7HEs-hxa3pV9EI-nV7MI")!
         
+        let json: [String : Any] = ["longDynamicLink" : "\(longDynamicLink)"]
         
+        let headers = ["Content-Type": "application/json"]
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+    
+        request.allHTTPHeaderFields = headers
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        request.httpBody = jsonData
+        
+        session.dataTask(with: request) { (data, response, error) in
+            if (error != nil) {
+                print(error!)
+                completion(.success(longDynamicLink))
+                   } else {
+                       //let httpResponse = response as? HTTPURLResponse
+                       //print(httpResponse!)
+
+                       do {
+                           if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String : Any]{
+                               guard let shortDynamicLink = json["shortLink"] as? String else {return}
+                               let shortDynamicLinkURL = URL(string: shortDynamicLink)!
+                               completion(.success(shortDynamicLinkURL))
+                           }
+                       } catch {
+                           completion(.success(longDynamicLink))
+                       }
+
+                   }
+        }.resume()
     }
 }
