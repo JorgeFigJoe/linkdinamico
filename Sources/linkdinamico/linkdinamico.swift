98acc27d1d7d3f1f27319e2470bac32c4135c434
Jorge Figgit config --global user.emailgit config --global user.name Jorge <sanchezjorf@globalhitss.com>
2021-11-16 12:27:42 -0600

    add

--- Sources/linkdinamico/linkdinamico.swift
+++ Sources/linkdinamico/linkdinamico.swift
@@ -102,7 +102,7 @@ import UIKit
      }
      
      public func openModule(view : UIView) -> UIView{
-         let controls = ControlConference(frame: view.bounds)
+         let controls = ControlConference(frame: CGRectMake(0 , 0, view.frame.width, view.frame.height * 0.7) )
          //view.addSubview(controls)
          return controls
          
