c421580589240bcde653c87547dc227a930e2daa
Jorge Figgit config --global user.emailgit config --global user.name Jorge <sanchezjorf@globalhitss.com>
2021-11-16 12:59:14 -0600

    add6

--- Sources/linkdinamico/ControlConference.swift
+++ Sources/linkdinamico/ControlConference.swift
@@ -12,11 +12,18 @@ class ControlConference : UIView {
     
     override init (frame : CGRect) {
         super.init(frame : frame)
+        self.commonInitialization()
         
     }
     
     required public init?(coder aDecoder: NSCoder) {
       super.init(coder: aDecoder)
+    }
+    
+    @objc func commonInitialization(){
+      let view = Bundle.main.loadNibNamed("ControlConference", owner: self, options: nil)?.first as! UIView
+      view.frame = self.bounds
+      self.addSubview(view)
     }
     
     
