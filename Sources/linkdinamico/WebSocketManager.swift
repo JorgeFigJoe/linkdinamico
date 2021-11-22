763aaa86664e3cd7167dc7b70ea5ddfa71c0ece0
Jorge Figgit config --global user.emailgit config --global user.name Jorge <sanchezjorf@globalhitss.com>
2021-11-22 16:07:35 -0600

    add logs

--- Sources/linkdinamico/WebSocketManager.swift
+++ Sources/linkdinamico/WebSocketManager.swift
@@ -27,6 +27,28 @@ class WebSocketManager {
 extension WebSocketManager : WebSocketDelegate {
     func didReceive(event: WebSocketEvent, client: WebSocket) {
         print(event)
+        switch event {
+            case .connected(let headers):
+                print("websocket is connected: \(headers)")
+            case .disconnected(let reason, let code):
+                print("websocket is disconnected: \(reason) with code: \(code)")
+            case .text(let string):
+                print("Received text: \(string)")
+            case .binary(let data):
+                print("Received data: \(data.count)")
+            case .ping(_):
+                break
+            case .pong(_):
+                break
+            case .viabilityChanged(_):
+                break
+            case .reconnectSuggested(_):
+                break
+            case .cancelled:
+                print("websocket is cancelled")
+            case .error(let error):
+                print("websocket is not connected: \(error)")
+            }
     }
     
 }
