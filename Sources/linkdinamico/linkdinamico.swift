ba13e50f1bf0409388c8d2fb386dd0f9bbb52b3a
Jorge Figgit config --global user.emailgit config --global user.name Jorge <sanchezjorf@globalhitss.com>
2021-11-27 12:09:00 -0600

    fix views

--- Sources/linkdinamico/linkdinamico.swift
+++ Sources/linkdinamico/linkdinamico.swift
@@ -5,7 +5,7 @@ import UIKit
      
      let webSocket = WebSocketManager()
      fileprivate var jsonn : [String : Any]?
-     var controls: ControlConference?
+     var controls = ControlConference()
         
     public init() {
     
@@ -124,14 +124,14 @@ import UIKit
      
      public func openModule(view : UIView) -> UIView?{
          controls = ControlConference(frame: CGRect(x: 8 , y: 100, width: view.frame.width/2, height: view.frame.width/2) )
-         if let controlsView = controls{
-             controlsView.delegate = self
-             controlsView.webSocket = self.webSocket
-             view.addSubview(controlsView)
-             return controlsView
-         }
+         //if let controlsView = controls{
+             controls.delegate = self
+             controls.webSocket = self.webSocket
+             view.addSubview(controls)
+             return controls
+        // }
 
-         return nil
+         //return nil
      }
 
 }
