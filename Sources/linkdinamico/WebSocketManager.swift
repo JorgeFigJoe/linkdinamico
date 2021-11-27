e4f7bb29c545b17a77171f2c1f9f77df0e3e7efc
Jorge Figgit config --global user.emailgit config --global user.name Jorge <sanchezjorf@globalhitss.com>
2021-11-27 12:26:18 -0600

    fix json2

--- Sources/linkdinamico/WebSocketManager.swift
+++ Sources/linkdinamico/WebSocketManager.swift
@@ -61,7 +61,7 @@ class WebSocketManager {
                     {
                       "action":"command",
                       "payload":{
-                          "command": "HANGUP",
+                          "command": HANGUP,
                           "room": "\(self.identifier)",
                           "extraData": null,
                           "connectionType": "ios-integration-client"
